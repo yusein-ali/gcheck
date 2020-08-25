9ee3a2cbbe61cc2691dc29ca1d29a779cd2b48e9
Tuomas Laine <tuomaslaine97@gmail.com>
2020-08-25 16:48:15 +0300

    add Deleter and Freer for postponing pointer freeing

---
+++ include/gcheck/deleter.h
@@ -0,0 +1,62 @@
+#pragma once
+
+template<typename T>
+class Deleter {
+public:
+    Deleter() {}
+    ~Deleter() { Clear(); }
+    template<typename... Args>
+    T* New(Args&&... args) {
+        auto ptr = new T(std::forward<Args>(args)...);
+        Add(ptr);
+        return ptr;
+    }
+    void Add(T* ptr) { pointers_.push_back(ptr); }
+    void Clear() {
+        for(auto ptr : pointers_)
+            delete ptr;
+        pointers_.clear();
+    }
+private:
+    std::vector<T*> pointers_;
+};
+
+template<typename T>
+class Deleter<T[]> {
+public:
+    Deleter() {}
+    ~Deleter() {
+        for(auto ptr : pointers_)
+            delete[] ptr;
+    }
+    T* New(size_t n) {
+        auto ptr = new T[n];
+        Add(ptr);
+        return ptr;
+    }
+    void Add(T* ptr) {
+        pointers_.push_back(ptr);
+    }
+private:
+    std::vector<T*> pointers_;
+};
+
+template<typename T>
+class Freer {
+public:
+    Freer() {}
+    ~Freer() { Clear(); }
+    T* New(size_t n) {
+        auto ptr = (T*)malloc(sizeof(T)*n);
+        Add(ptr);
+        return ptr;
+    }
+    void Add(T* ptr) { pointers_.push_back(ptr); }
+    void Clear() {
+        for(auto ptr : pointers_)
+            free(ptr);
+        pointers_.clear();
+    }
+private:
+    std::vector<T*> pointers_;
+};
