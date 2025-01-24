ec34b8ee20411040031a71637f527f800e3aa53f
Yusein Ali <yusein.ali@aalto.fi>
2025-01-24 10:59:13 +0200

    refactor: reorganize includes and improve formatting in user_object.h

--- include/user_object.h
+++ include/user_object.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#include <string>
-#include <unordered_map>
-#include <vector>
-#include <type_traits>
-#include <sstream>
-#include <utility>
 #include <optional>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 
 #include "argument.h"
 #include "json.h"
@@ -21,49 +21,58 @@ namespace gcheck {
  */
 template<template<typename> class allocator = std::allocator>
 class _UserObject {
-    typedef std::basic_string<char, std::char_traits<char>, allocator<char>> stdstring;
+	typedef std::basic_string<char, std::char_traits<char>, allocator<char>> stdstring;
+
 public:
-    _UserObject() {}
-    _UserObject(const _UserObject& v) = default;
+	_UserObject() {}
+	_UserObject(const _UserObject &v) = default;
 
-    template<template<typename> class T>
-    _UserObject(const _UserObject<T>& uo) :
-        as_string_(uo.string()),
-        as_json_(uo.json())
+	template<template<typename> class T>
+	_UserObject(const _UserObject<T> &uo) :
+		as_string_(uo.string()), as_json_(uo.json())
 #ifdef GCHECK_CONSTRUCT_DATA
-        , construct_(uo.construct())
+		,
+		construct_(uo.construct())
 #endif
-        {}
+	{
+	}
 
-    template<template<typename> class T>
-    _UserObject(const std::optional<_UserObject<T>>& item) = delete;
-    template<typename T>
-    _UserObject(const T& item) {
-        as_json_ = item;
-        as_string_ = toString(item);
+	template<template<typename> class T>
+	_UserObject(const std::optional<_UserObject<T>> &item) = delete;
+	template<typename T>
+	_UserObject(const T &item) {
+		as_json_ = item;
+		as_string_ = toString(item);
 #ifdef GCHECK_CONSTRUCT_DATA
-        construct_ = toConstruct(item);
+		construct_ = toConstruct(item);
 #endif
-    }
-    template<typename... Args>
-    _UserObject(const Args&... items) : _UserObject(std::tuple<Args...>(items...)) {}
+	}
+	template<typename... Args>
+	_UserObject(const Args &...items) : _UserObject(std::tuple<Args...>(items...)) {}
 
-    JSON json() const { return as_json_; }
-    std::string string() const { return (std::string)as_string_; }
+	JSON json() const {
+		return as_json_;
+	}
+	std::string string() const {
+		return (std::string) as_string_;
+	}
 #ifdef GCHECK_CONSTRUCT_DATA
-    std::string construct() const { return (std::string)construct_; }
+	std::string construct() const {
+		return (std::string) construct_;
+	}
 #endif
 
-    template<typename T>
-    _UserObject& operator=(const T& v) {
-        return *this = _UserObject(v);
-    }
+	template<typename T>
+	_UserObject &operator=(const T &v) {
+		return *this = _UserObject(v);
+	}
+
 private:
-    stdstring as_string_;
-    _JSON<allocator> as_json_;
+	stdstring as_string_;
+	_JSON<allocator> as_json_;
 #ifdef GCHECK_CONSTRUCT_DATA
-    stdstring construct_; // a string representation on how to construct the object e.g. "std::vector<int>({0, 1, 2})"
+	stdstring construct_; // a string representation on how to construct the object e.g. "std::vector<int>({0, 1, 2})"
 #endif
 };
 
-} // gcheck
+} // namespace gcheck
